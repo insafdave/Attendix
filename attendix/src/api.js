1c139cae828f0b4fb0a341d7634c0942660ec784
insafdave <insaffizu@gmail.com>
2026-09-12 16:14:18 +0530

    connect frontend to production backend

--- attendix/src/api.js
+++ attendix/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://attendix-s2k.onrender.com/api";
 
 function getAuthHeaders() {
   const token = localStorage.getItem("attendix_token");
